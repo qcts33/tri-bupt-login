b72b65a8a08edc04ff42ab2e6c6d66d341b8cd98
qcts33 <qcthreestones@gmail.com>
2019-05-21 21:10:28 +0800

    gui login

---
+++ ngw_bupt.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+import PySimpleGUI as sg
+
+
+def login(auth):
+    login_url = "http://ngw.bupt.edu.cn/login"
+    res = requests.post(login_url, auth)
+    soup = BeautifulSoup(res.text, "lxml")
+    try:
+        result = soup.find("h3").text
+    except AttributeError:
+        result = soup.find("div", {"class": "ui error message"}).text
+    return result.strip()
+
+
+def gui():
+    with open("bupt_login.json", "r") as fp:
+        data = json.load(fp)
+    layout = [
+        [sg.Text("Please Select the auth info")],
+        [sg.InputCombo(tuple(data.keys()), size=(20, 1))],
+        [sg.Button("Login"), sg.Button("Logout")],
+    ]
+    window = sg.Window("Login", layout)
+    while True:
+        event, values = window.Read()
+        if event is None:
+            break
+        if event == "Login":
+            name = values[0]
+            auth = data[name]
+            result = login(auth)
+            sg.Popup(result)
+        if event == "Logout":
+            requests.get("http://ngw.bupt.edu.cn/logout")
+            sg.Popup("已登出")
+
+
+if __name__ == "__main__":
+    gui()
